b42a7afbe9b917c9b17d37d053dc9ee17baf6bac
Mahantesh kumar <97968290+mantu-9194@users.noreply.github.com>
2022-07-11 17:45:24 +0530

    Update Bollywood.jsx

--- src/components/Bollywood.jsx
+++ src/components/Bollywood.jsx
@@ -7,7 +7,7 @@ import axios from "axios";
 const Bollywood = () => {
   const [data, setData] = useState([])
 useEffect(()=>{
-  axios.get("http://localhost:8000/api/bolly")
+  axios.get("https://dashboard.heroku.com/apps/backend-blog-react111/api/bolly")
   .then((res)=>{
     setData(res.data);
   })
